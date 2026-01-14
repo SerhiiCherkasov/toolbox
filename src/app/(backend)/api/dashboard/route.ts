807d41c6cbbb127d06666bc1f3a913d2b6d0841b
SerhiiCherkasov <42519720+SerhiiCherkasov@users.noreply.github.com>
2026-01-14 20:04:54 +0200

    a bit of refactor

--- src/app/(backend)/api/dashboard/route.ts
+++ src/app/(backend)/api/dashboard/route.ts
@@ -1,18 +1,18 @@
 import type { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
-  return Response.json({ message: "Hello World" });
+  return Response.json({ message: "Hello World!!!" });
 }
 
-export async function HEAD(request: NextRequest) {}
+// export async function HEAD(request: NextRequest) {}
 
 export async function POST(request: NextRequest) {}
 
-export async function PUT(request: NextRequest) {}
+// export async function PUT(request: NextRequest) {}
 
-export async function DELETE(request: NextRequest) {}
+// export async function DELETE(request: NextRequest) {}
 
-export async function PATCH(request: NextRequest) {}
+// export async function PATCH(request: NextRequest) {}
 
 // If `OPTIONS` is not defined, Next.js will automatically implement `OPTIONS` and set the appropriate Response `Allow` header depending on the other methods defined in the Route Handler.
-export async function OPTIONS(request: NextRequest) {}
+// export async function OPTIONS(request: NextRequest) {}
